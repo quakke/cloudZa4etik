e62ea81556c528e94377200df7a7c99c55bbb109
quakke <gatchina.irka@mail.ru>
2016-04-28 00:51:57 +0300

    created app

---
+++ cloudscloudsclouds/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cloudscloudsclouds.MvcApplication" Language="C#" %>
---
+++ cloudscloudsclouds/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cloudscloudsclouds.Startup))]
+namespace cloudscloudsclouds
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
